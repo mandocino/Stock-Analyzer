c09a47832ac90c3cf42993648d6118cda4a81021
Cinodevv <96893418+Cinodevv@users.noreply.github.com>
2024-08-30 20:09:20 -0400

    feat: create stock analyzed
    Create the page for after you search to display information.
    Remove unnecessary code

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+import yfinance as yahooFinance
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('analyzerHomepage.html')
+
+@app.route('/analyze/<query>', methods=['GET'])
+def analyze(query):
+    GetTickerInformation = None
+    try:
+        app.logger.debug(query)
+        GetTickerInformation = yahooFinance.Ticker(query)
+        return render_template('stockAnalyzed.html', GetTickerInformation=GetTickerInformation)
+    except:
+        return render_template('homepage.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
